bd30409bb1e4e87cb7be6c85ce8869696799c0a0
haijie yin <2919276084@qq.com>
2026-05-19 16:37:37 +0800

    fix 暂时放弃零拷贝代码

--- yolov8/yolov8.h
+++ yolov8/yolov8.h
@@ -10,12 +10,6 @@ typedef struct
     rknn_input_output_num io_num;
     rknn_tensor_attr*     input_attrs;
     rknn_tensor_attr*     output_attrs;
-#if defined(ZERO_COPY)
-    rknn_tensor_mem*  input_mems[1];
-    rknn_tensor_mem*  output_mems[9];
-    rknn_tensor_attr* input_native_attrs;
-    rknn_tensor_attr* output_native_attrs;
-#endif
     int  model_channel;
     int  model_width;
     int  model_height;
