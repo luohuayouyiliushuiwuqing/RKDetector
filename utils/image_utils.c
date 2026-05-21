3d087f15ff05d22a4b1d22a1c1ba5a29575ece1a
haijie yin <2919276084@qq.com>
2026-05-21 11:44:14 +0800

    移除 fileutils 库依赖：更新 CMakeLists.txt，简化项目结构

--- utils/image_utils.c
+++ utils/image_utils.c
@@ -5,12 +5,6 @@
 #include "im2d.h"
 #include "drmrga.h"
 
-#define STB_IMAGE_IMPLEMENTATION
-#define STBI_NO_THREAD_LOCALS
-#define STBI_ONLY_JPEG
-#define STBI_ONLY_PNG
-#include "stb_image.h"
-
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
